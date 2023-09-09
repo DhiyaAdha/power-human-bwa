7f3199b23304cef7ad9da257bfed9edbb37e4461
Dhiya <adiadiadha@google.com>
2023-09-09 22:10:21 +0700

    up first migrate, livewire, fillable model first

---
+++ database/migrations/2023_09_09_142241_create_employees_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('gender')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('photo')->nullable();
+            $table->integer('age')->nullable();
+
+            $table->bigInteger('team_id')->unsigned();
+            $table->bigInteger('role_id')->unsigned();
+
+            $table->boolean('is_verified')->default(false);
+            $table->timestamp('verified_at')->nullable();
+
+            // $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            // $table->foreign('team_id')->references('id')->on('companies')->onDelete('cascade');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
