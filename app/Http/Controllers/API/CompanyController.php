52ce505871f3dbb9ddcae97bceb3fa844b59087d
Dhiya <adiadiadha@google.com>
2023-09-15 04:15:16 +0700

    create-request company fisrt vid 9

--- app/Http/Controllers/API/CompanyController.php
+++ app/Http/Controllers/API/CompanyController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers\API;
 
+use Exception;
+use App\Models\User;
 use App\Models\Company;
 use Illuminate\Http\Request;
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\CreateCompanyRequest;
 
 class CompanyController extends Controller
 {
